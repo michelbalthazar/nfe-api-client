ae9b4933ef9216295fabc244710822eb28666393
Michel Balthazar <michel@nfe.io>
2018-03-28 13:57:56 -0300

    Create initial colutions

---
+++ nfe-api-client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nfe_api_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
