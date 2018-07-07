0a5c36d2726bee622b4d7fad8226dad065c29030
Michel Balthazar <michel@nfe.io>
2018-07-07 13:31:09 -0300

    Add unitTests solution

---
+++ UnitTests/InvoiceClient.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tests.UnitTests
+{
+    public class InvoiceClient
+    {
+        [Fact]
+        public void PostAsync_WhenSendValidJson_ReturnsOk()
+        {
+
+        }
+    }
+}
