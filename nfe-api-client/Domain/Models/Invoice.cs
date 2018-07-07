19080e29ffb9e76d414ebb67db25686e058c5b4d
Michel Balthazar <michel@nfe.io>
2018-07-07 12:37:38 -0300

    Fix invoice's properties

--- nfe-api-client/Domain/Models/Invoice.cs
+++ nfe-api-client/Domain/Models/Invoice.cs
@@ -2,10 +2,33 @@
 {
     public class Invoice
     {
-        private string CityServiceCode;
-        private string FederalServiceCode;
-        private string CnaeCode;
-        private string Description;
-        private double ServicesAmount;
+        /// <summary>
+        /// Emitente da NFS-e
+        /// </summary>
+        private Issuer Issuer { get; set; }
+        /// <summary>
+        /// <summary>Tomador do serviço</summary>
+        /// </summary>
+        private Borrower Borrower { get; set; }
+        /// <summary>
+        /// Código municipal do serviço prestado (cada cidade possuí sua tabela)
+        /// </summary>
+        private string CityServiceCode { get; set; }
+        /// <summary>
+        /// Código federal do serviço prestado (cada estado possuí sua tabela)
+        /// </summary>
+        private string FederalServiceCode { get; set; }
+        /// <summary>
+        /// Atividades da Empresa (CNAE)
+        /// </summary>
+        private string EconomicActivitieCode { get; set; }
+        /// <summary>
+        /// Descrição do serviço prestado
+        /// </summary>
+        private string Description { get; set; }
+        /// <summary>
+        /// Valor total do serviço prestado
+        /// </summary>
+        private double ServicesAmount { get; set; }
     }
 }
