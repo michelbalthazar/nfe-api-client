0446bb90280930ce3577ee4c63f3d2a6b05a8bf4
Michel Balthazar <michel@nfe.io>
2018-07-07 13:21:45 -0300

    Initicial invoiceClient's postAsync integrationTest

---
+++ IntegrationTests/InvoiceClient.cs
@@ -0,0 +1,52 @@
+using nfe.api.client.Domain.Models;
+using System;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class InvoiceClient
+    {
+        private readonly GetAppSettings _settingsApp = new GetAppSettings();
+
+        [Fact]
+        public void PostAsync_WhenSendValidJson_ReturnOk()
+        {
+            // arrange
+            var apiKey = _settingsApp.Configuration["Authentication:ApiKey"];
+            var companyIdSP = _settingsApp.Configuration["Authentication:CompanyId"];
+            //var client = new ServiceInvoicesClient();
+            //var invoice = new InvoiceService(client);
+            var item = new Invoice
+            {
+                CityServiceCode = "3093",
+                Description = "TESTE EMISSAO",
+                ServicesAmount = 0.01,
+                Borrower = new Borrower
+                {
+                    FederalTaxNumber = 191,
+                    Name = "BANCO DO BRASIL SA",
+                    Email = "email@remetente",
+                    Address = new Address
+                    {
+                        Country = "BRA",
+                        PostalCode = "70073901",
+                        Street = "Outros Quadra 1 Bloco G Lote 32",
+                        Number = "S/N",
+                        AdditionalInformation = "QUADRA 01 BLOCO G",
+                        District = "Asa Sul",
+                        CityCode = "5300108",
+                        CityName = "Brasilia",
+                        State = "DF"
+                    },
+                },
+
+            };
+
+            // act
+            //var result = await invoice.Create(companyIdSP, apiKey, item);
+
+            // asser
+            //Assert.NotNull(result);
+        }
+    }
+}
