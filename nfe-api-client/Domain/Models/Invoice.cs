d8628e7b4e96e1dbe81aa424e01af339f6c3829e
Michel Balthazar <michel@nfe.io>
2018-07-07 13:22:01 -0300

    Set invoice's properties public access

--- nfe-api-client/Domain/Models/Invoice.cs
+++ nfe-api-client/Domain/Models/Invoice.cs
@@ -5,30 +5,30 @@
         /// <summary>
         /// Emitente da NFS-e
         /// </summary>
-        private Issuer Issuer { get; set; }
+        public Issuer Issuer { get; set; }
         /// <summary>
         /// <summary>Tomador do serviço</summary>
         /// </summary>
-        private Borrower Borrower { get; set; }
+        public Borrower Borrower { get; set; }
         /// <summary>
         /// Código municipal do serviço prestado (cada cidade possuí sua tabela)
         /// </summary>
-        private string CityServiceCode { get; set; }
+        public string CityServiceCode { get; set; }
         /// <summary>
         /// Código federal do serviço prestado (cada estado possuí sua tabela)
         /// </summary>
-        private string FederalServiceCode { get; set; }
+        public string FederalServiceCode { get; set; }
         /// <summary>
         /// Atividades da Empresa (CNAE)
         /// </summary>
-        private string EconomicActivitieCode { get; set; }
+        public string EconomicActivitieCode { get; set; }
         /// <summary>
         /// Descrição do serviço prestado
         /// </summary>
-        private string Description { get; set; }
+        public string Description { get; set; }
         /// <summary>
         /// Valor total do serviço prestado
         /// </summary>
-        private double ServicesAmount { get; set; }
+        public double ServicesAmount { get; set; }
     }
 }
