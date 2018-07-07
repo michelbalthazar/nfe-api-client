7e3a28c935e458bbcca1656ca5af5cc29fed3f39
Michel Balthazar <michel@nfe.io>
2018-07-07 13:20:59 -0300

    Implement GetappSettings helper

---
+++ IntegrationTests/GetAppSettings.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+namespace IntegrationTests
+{
+    public class GetAppSettings
+    {
+        public readonly IConfigurationRoot Configuration;
+
+        public GetAppSettings()
+        {
+            var builder = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json", optional: true)
+               .AddJsonFile($"appsettings.development.json");
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+    }
+}
