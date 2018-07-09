9ed6db758b3050c0a69375b1ff7821d6826c97ca
Michel Balthazar <michel@nfe.io>
2018-07-09 08:53:40 -0300

    implement invoiceResource

--- nfe-api-client/Domain/Models/Enums.cs
+++ nfe-api-client/Domain/Models/Enums.cs
@@ -108,4 +108,175 @@ namespace ServiceInvoice.Domain.Models
         [Display(Name = "Microempresario e Empresa de Pequeno Porte")]
         MicroempresarioEmpresaPequenoPorte = 6
     }
+
+    /// <summary>
+    /// Status da invoice no processamento do sistema
+    /// </summary>
+    public enum NotaFiscalFlowStatus
+    {
+        CancelFailed = -2,
+        IssueFailed = -1,
+        Issued = 1,
+        Cancelled = 2,
+        PullFromCityHall = 3,
+
+        WaitingCalculateTaxes = 10,
+        WaitingDefineRpsNumber = 11,
+        WaitingSend = 12,
+        WaitingSendCancel = 13,
+        WaitingReturn = 14,
+        WaitingDownload = 15
+    }
+
+    /// <summary>
+    /// Status da invoice 
+    /// </summary>
+    public enum InvoiceStatus
+    {
+        Error = -1,
+        None = 0,
+        Created = 1,
+        Issued = 2,
+        Cancelled = 3
+    }
+
+    /// <summary>
+    /// RPS   = Recibo Provisório de Serviços
+    /// RPS-M = Recibo Provisório de Serviços proveniente de Nota Fiscal Conjugada (Mista)
+    /// RPS-C = Cupom
+    /// </summary>
+    public enum RpsType
+    {
+        /// <summary>
+        /// Recibo Provisório de Serviços
+        /// </summary>
+        Rps = 1,
+
+        /// <summary>
+        /// Recibo Provisório de Serviços proveniente de Nota Fiscal Conjugada (Mista)
+        /// </summary>
+        RpsMista = 2,
+
+        /// <summary>
+        /// Cupom
+        /// </summary>
+        Cupom = 4
+    }
+
+    /// <summary>
+    /// N = Normal
+    /// C = Cancelada
+    /// E = Extraviada
+    /// </summary>
+    public enum RpsStatus
+    {
+        /// <summary>
+        /// Normal
+        /// </summary>
+        Normal = 1,
+
+        /// <summary>
+        /// Cancelada
+        /// </summary>
+        Canceled = 2,
+
+        /// <summary>
+        /// Extraviada
+        /// </summary>
+        Lost = 4
+    }
+
+    /// <summary>
+    /// T = Tributação no município
+    /// F = Tributação fora do município
+    /// E = Exportação
+    /// I = Isento
+    /// I = Imune
+    /// J = Exigibilidade suspensa por decisão judicial
+    /// A = Exigibilidade suspensa por procedimento administrativo
+    /// </summary>
+    public enum TaxationType
+    {
+        /// <summary>
+        /// Nenhum
+        /// </summary>
+        [Display(Name = "Nenhum")]
+        None = 0,
+
+        /// <summary>
+        /// Tributação dentro do mesmo município
+        /// </summary>
+        [Display(Name = "Tributação dentro do mesmo município")]
+        WithinCity = 1,
+
+        /// <summary>
+        /// Tributação fora do município
+        /// </summary>
+        [Display(Name = "Tributação fora do município")]
+        OutsideCity = 2,
+
+        /// <summary>
+        /// Exportação
+        /// </summary>
+        [Display(Name = "Exportação")]
+        Export = 4,
+
+        /// <summary>
+        /// Isento
+        /// </summary>
+        [Display(Name = "Isento")]
+        Free = 8,
+
+        /// <summary>
+        /// Imune
+        /// </summary>
+        [Display(Name = "Imune")]
+        Immune = 16,
+
+        /// <summary>
+        /// Exigibilidade suspensa por decisão judicial
+        /// </summary>
+        [Display(Name = "Suspensa por decisão judicial")]
+        SuspendedCourtDecision = 32,
+
+        /// <summary>
+        /// Exigibilidade suspensa por procedimento administrativo
+        /// </summary>
+        [Display(Name = "Suspensa por procedimento administrativo")]
+        SuspendedAdministrativeProcedure = 64,
+
+        /// <summary>
+        /// Tributação fora do município porém isento
+        /// </summary>
+        [Display(Name = "Tributação fora do município porém isento")]
+        OutsideCityFree = OutsideCity | Free,
+
+        /// <summary>
+        /// Tributação fora do município porém imune
+        /// </summary>
+        [Display(Name = "Tributação fora do município porém imune")]
+        OutsideCityImmune = OutsideCity | Immune,
+
+        /// <summary>
+        /// Tributação fora do município porém suspensa
+        /// </summary>
+        [Display(Name = "Tributação fora do município porém suspensa")]
+        OutsideCitySuspended = SuspendedCourtDecision | Free,
+
+        /// <summary>
+        /// Tributação fora do município porém suspensa
+        /// </summary>
+        [Display(Name = "Tributação fora do município porém suspensa por procedimento administrativo")]
+        OutsideCitySuspendedAdministrativeProcedure = SuspendedAdministrativeProcedure | Free,
+    }
+
+    public enum ApiEnvironment
+    {
+        [Display(Name = "Development")]
+        Development = 0,
+        [Display(Name = "Production")]
+        Production = 1,
+        [Display(Name = "Staging")]
+        Staging = 2
+    }
 }
--- nfe-api-client/Domain/Models/InvoiceResource.cs
+++ nfe-api-client/Domain/Models/InvoiceResource.cs
@@ -1,6 +1,207 @@
-﻿namespace ServiceInvoice.Domain.Models
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceInvoice.Domain.Models
 {
     public class InvoiceResource
     {
+        /// <summary>
+        /// Prestador dos serviços
+        /// </summary>
+        public LegalPerson Provider { get; set; }
+        /// <summary>
+        /// Tomador dos serviços 
+        /// </summary>
+        public Person Borrower { get; set; }
+        /// <summary>
+        /// Código municipal do serviço prestado (cada cidade possuí sua tabela)
+        /// </summary>
+        public string CityServiceCode { get; set; }
+        /// <summary>
+        /// Código federal do serviço prestado (cada estado possuí sua tabela)
+        /// </summary>
+        public string FederalServiceCode { get; set; }
+        /// <summary>
+        /// Atividades da Empresa (CNAE)
+        /// </summary>
+        public string EconomicActivitieCode { get; set; }
+        /// <summary>
+        /// Descrição do serviço prestado
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Número da RPS
+        /// </summary>
+        public long RpsNumber { get; set; }
+        /// <summary>
+        /// Número de serie da RPS
+        /// </summary>
+        public string RpsSerialNumber { get; set; }
+        /// <summary>
+        /// Valor total do serviço prestado
+        /// </summary>
+        public double ServicesAmount { get; set; }
+        /// <summary>
+        /// Identificação
+        /// </summary>
+        public virtual string Id { get; set; }
+        /// <summary>
+        /// Ambiente de Processamento
+        /// </summary>
+        [Required]
+        public ApiEnvironment Environment { get; set; }
+        /// <summary>
+        /// Status do processamento
+        /// </summary>
+        public NotaFiscalFlowStatus FlowStatus { get; set; }
+        /// <summary>
+        /// Mensagem de processamento
+        /// </summary>
+        public string FlowMessage { get; set; }
+        /// <summary>
+        /// Número do lote da RPS
+        /// </summary>
+        public long BatchNumber { get; set; }
+        /// <summary>
+        /// Número do protocolo do lote da RPS
+        /// </summary>
+        public string BatchCheckNumber { get; set; }
+        /// <summary>
+        /// Número do NFE
+        /// </summary>
+        public long Number { get; set; }
+        /// <summary>
+        /// Código de Verificação da NFE
+        /// </summary>
+        public string CheckCode { get; set; }
+        /// <summary>
+        /// Status da NFE
+        /// </summary>
+        public InvoiceStatus Status { get; set; }
+        /// <summary>
+        /// Tipo da RPS
+        /// </summary>
+        public RpsType RpsType { get; set; }
+        /// <summary>
+        /// Status da RPS
+        /// </summary>
+        public RpsStatus RpsStatus { get; set; }
+        /// <summary>
+        /// Tipo da tributação
+        /// </summary>
+        public TaxationType TaxationType { get; set; }
+        /// <summary>
+        /// Valor de deduções
+        /// </summary>
+        public decimal DeductionsAmount { get; set; }
+        /// <summary>
+        /// Valor do desconto incondicionado
+        /// </summary>
+        public decimal DiscountUnconditionedAmount { get; set; }
+        /// <summary>
+        /// Valor do desconto condicionado
+        /// </summary>
+        public decimal DiscountConditionedAmount { get; set; }
+        /// <summary>
+        /// Valor da base de calculo de impostos
+        /// </summary>
+        public decimal BaseTaxAmount { get; set; }
+        /// <summary>
+        /// Aliquota do ISS
+        /// </summary>
+        public decimal IssRate { get; set; }
+        /// <summary>
+        /// Valor do ISS
+        /// </summary>
+        public decimal IssTaxAmount { get; set; }
+        /// <summary>
+        /// Valor retido do Imposto de Renda (IR)
+        /// </summary>
+        public decimal IrAmountWithheld { get; set; }
+        /// <summary>
+        /// Valor retido do PIS
+        /// </summary>
+        public decimal PisAmountWithheld { get; set; }
+        /// <summary>
+        /// Valor retido do COFINS
+        /// </summary>
+        public decimal CofinsAmountWithheld { get; set; }
+        /// <summary>
+        /// Valor retido do CSLL
+        /// </summary>
+        public decimal CsllAmountWithheld { get; set; }
+        /// <summary>
+        /// Valor retido do INSS
+        /// </summary>
+        public decimal InssAmountWithheld { get; set; }
+        /// <summary>
+        /// Valor retido do ISS
+        /// </summary>
+        public decimal IssAmountWithheld { get; set; }
+        /// <summary>
+        /// Valor de outras retenções
+        /// </summary>
+        public decimal OthersAmountWithheld { get; set; }
+        /// <summary>
+        /// Valor das retenções
+        /// </summary>
+        public decimal AmountWithheld { get; set; }
+        /// <summary>
+        /// Valor líquido
+        /// </summary>
+        public decimal AmountNet { get; set; }
+        /// <summary>
+        /// Tributos aproximados
+        /// </summary>
+        public InvoiceApproximateTaxes ApproximateTax { get; set; }
+        /// <summary>
+        /// Informações Adicionais
+        /// </summary>
+        public string AdditionalInformation { get; set; }
+        /// <summary>
+        /// Data de criação
+        /// </summary>
+        public virtual DateTimeOffset CreatedOn { get; set; }
+        /// <summary>
+        /// Data da última modificação
+        /// </summary>
+        public virtual DateTimeOffset? ModifiedOn { get; set; }
+        /// <summary>
+        /// Data de cancelamento
+        /// </summary>
+        public virtual DateTimeOffset? CancelledOn { get; set; }
+        /// <summary>
+        /// Data de emissão
+        /// </summary>
+        public DateTimeOffset IssuedOn { get; set; }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public static Invoice FromJson(string data)
+        {
+            return JsonConvert.DeserializeObject<Invoice>(data);
+        }
+    }
+
+    public class InvoiceApproximateTaxes
+    {
+        /// <summary>
+        /// Nome da fonte da taxa
+        /// </summary>
+        public string Source { get; set; }
+
+        /// <summary>
+        /// Versão da taxa baseado na fonte
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// Taxa dos tributos aproximados
+        /// </summary>
+        public decimal TotalRate { get; set; }
     }
 }
