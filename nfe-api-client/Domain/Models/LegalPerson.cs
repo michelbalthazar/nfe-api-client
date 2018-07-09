71f247bdf028a8f7cc59fbd686557eff9f0b7d19
Michel Balthazar <michel@nfe.io>
2018-07-09 10:17:30 -0300

    Remove empty space on LegalPerson class

--- nfe-api-client/Domain/Models/LegalPerson.cs
+++ nfe-api-client/Domain/Models/LegalPerson.cs
@@ -12,42 +12,38 @@ namespace ServiceInvoice.Domain.Models
         /// Nome Fantasia (esse nome será usado no assunto do e-mail)
         /// </summary>
         public string TradeName { get; set; }
-
         /// <summary>
         /// Número de Inscricação na Prefeitura (CCM) 
         /// </summary>
         public string MunicipalTaxNumber { get; set; }
-
         /// <summary>
         /// Regime de tributação
         /// </summary>
         public TaxRegime TaxRegime { get; set; }
-
         /// <summary>
         /// Regime especial de tributação
         /// </summary>
         public SpecialTaxRegime SpecialTaxRegime { get; set; }
-
+        /// <summary>
+        /// Código da Natureza Jurídica
+        /// </summary>
+        public LegalNature LegalNature { get; set; }
         /// <summary>
         /// Data de abertura da empresa
         /// </summary>
         public DateTime OpenningDate { get; set; }
-
         /// <summary>
         /// CNAEs da empresa
         /// </summary>
         public IEnumerable<EconomicActivities> EconomicActivities { get; set; }
-
         /// <summary>
         /// Número de Inscricação na Junta Comercial
         /// </summary>
         public long CompanyRegistryNumber { get; set; }
-
         /// <summary>
         /// Número de Inscricação na SEFAZ (IE)
         /// </summary>
         public long RegionalTaxNumber { get; set; }
-
         /// <summary>
         /// Taxa da Aliquota do ISS (Simples Nacional)
         /// </summary>
