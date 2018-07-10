2ae699e81b3c5d3ea99739371dfba7773e9b2574
Michel Balthazar <michel@nfe.io>
2018-07-10 09:43:23 -0300

    implement stringExtension class

---
+++ nfe-api-client/Domain/Common/StringExtension.cs
@@ -0,0 +1,23 @@
+﻿using ServiceInvoice.Domain.Models;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace System
+{
+    public static class StringExtension
+    {
+        // It is not OK yet!!
+        public static InvoiceResource XmlToInvoiceResource(this string xml)
+        {
+            var serializer = new XmlSerializer(typeof(InvoiceResource));
+            var rdr = new StringReader(xml);
+            return (InvoiceResource)serializer.Deserialize(rdr);
+        }
+
+        public static byte[] GetBytesFromString(this string value)
+        {
+            return Encoding.UTF8.GetBytes(value);
+        }
+    }
+}
