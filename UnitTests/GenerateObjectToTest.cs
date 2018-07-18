3bbc1bbe237f18651d4a37c2f3ba29d4322aae76
Michel Balthazar <michel@nfe.io>
2018-07-18 20:30:13 -0300

    change GenerateInvoiceToTest to GenerateObjectToTest

---
+++ UnitTests/GenerateObjectToTest.cs
@@ -0,0 +1,59 @@
+﻿using ServiceInvoice.Domain.Models;
+
+namespace Tests.UnitTests
+{
+    public static class GenerateObjectToTest
+    {
+        public static Invoice Invoice()
+        {
+            return new Invoice
+            {
+                CityServiceCode = "3093",
+                Description = "TESTE EMISSAO",
+                ServicesAmount = 0.01,
+                Borrower = new Person
+                {
+                    FederalTaxNumber = 191,
+                    Name = "BANCO DO BRASIL SA",
+                    Email = "email@remetente.com",
+                    Address = new Address
+                    {
+                        Country = "BRA",
+                        PostalCode = "70073901",
+                        Street = "Outros Quadra 1 Bloco G Lote 32",
+                        Number = "S/N",
+                        AdditionalInformation = "QUADRA 01 BLOCO G",
+                        District = "Asa Sul",
+                        City = new City { Code = "5300108", Name = "Brasilia" },
+                        State = "DF"
+                    },
+                },
+            };
+        }
+
+        public static LegalPerson Company()
+        {
+            return new LegalPerson
+            {
+                FederalTaxNumber = 05893631000281,
+                Name = "BANCO DO BRASIL SA",
+                TradeName = "BANCO DO BRASIL SA",
+                MunicipalTaxNumber = "12345",
+                TaxRegime = TaxRegime.SimplesNacional,
+                SpecialTaxRegime = SpecialTaxRegime.Nenhum,
+                Email = "email@deTest.com.br", // If set a valid email, this email will receive this one nfse's xml and pdf.
+                Address = new Address
+                {
+                    Country = "BRA",
+                    PostalCode = "70073901",
+                    Street = "Outros Quadra 1 Bloco G Lote 32",
+                    Number = "S/N", // optional
+                    AdditionalInformation = "QUADRA 01 BLOCO G", // optional
+                    District = "Asa Sul",
+                    City = new City { Code = "5300108", Name = "Brasília" },
+                    State = "DF"
+                },
+            };
+        }
+    }
+}
