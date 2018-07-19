2fb1fa78925d760cc1874139e1fe2b2e5ac54aca
Michel Balthazar <michel@nfe.io>
2018-07-19 14:45:58 -0300

    implement stringExtension's serialize and deserialize to json and object methods

--- nfe-api-client/Domain/Common/StringExtension.cs
+++ nfe-api-client/Domain/Common/StringExtension.cs
@@ -1,4 +1,5 @@
-﻿using ServiceInvoice.Domain.Models;
+﻿using Newtonsoft.Json;
+using ServiceInvoice.Domain.Models;
 using System.IO;
 using System.Text;
 using System.Xml.Serialization;
@@ -18,6 +19,16 @@ namespace System
         public static byte[] GetBytesFromString(this string value)
         {
             return Encoding.UTF8.GetBytes(value);
+        }
+
+        public static T JsonToObject<T>(this string json) where T : class
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public static string ToJson<T>(this object value)
+        {
+            return JsonConvert.SerializeObject(value);
         }
     }
 }
