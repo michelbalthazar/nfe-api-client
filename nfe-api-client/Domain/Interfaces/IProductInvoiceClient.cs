1605feed74fb1b836ad7d248812f6d4cb4b68467
Michel Balthazar <michel@nfe.io>
2019-12-23 22:12:36 -0200

    Create ProductInvoice Interface

---
+++ nfe-api-client/Domain/Interfaces/IProductInvoiceClient.cs
@@ -0,0 +1,55 @@
+﻿using nfe.api.client.Domain.Models.Product;
+using nfe.api.client.Domain.Models.Resource;
+using ServiceInvoice.Domain.Common;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceInvoice.Domain.Interfaces
+{
+    interface IProductInvoiceClient
+    {
+        /// <summary>Emitir uma Nota Fiscal de Produto(NFe)</summary>
+        /// <param name="company_id">ID da empresa</param>
+        /// <param name="item">Dados da nota fiscal de produto</param>
+        /// <returns>Nota Fiscal de Produto foi enviada com sucesso para fila de emissão</returns>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        Task<Result<InvoiceResource>> PostAsync(string company_id, Invoice item, CancellationToken cancellationToken);
+
+        /// <summary>Obter os detalhes de uma Nota Fiscal de Produto (NFe)</summary>
+        /// <param name="company_id">ID da empresa</param>
+        /// <param name="invoiceId">ID da Nota Fiscal de Produto (NFe)</param>
+        /// <returns>Sucesso na requisição</returns>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        Task<Result<InvoiceResource>> GetOneAsync(string company_id, string invoiceId, CancellationToken cancellationToken);
+
+        /// <summary>Listar as Notas Fiscais de Produto (NFe)</summary>
+        /// <param name="company_id">ID da empresa</param>
+        /// <param name="pageCount">Items por página</param>
+        /// <param name="pageIndex">Número da página</param>
+        /// <returns>Sucesso na requisição</returns>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        Task<Result<InvoiceResource>> GetAsync(string company_id, int? pageCount, int? pageIndex, CancellationToken cancellationToken);
+
+        /// <summary>Cancelar uma Nota Fiscal de Produto (NFe)</summary>
+        /// <param name="company_id">ID da empresa</param>
+        /// <param name="invoiceId">ID da Nota Fiscal de Produto (NFe)</param>
+        /// <returns>Nota fiscal cancelada com sucesso</returns>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        Task<Result<InvoiceResource>> DeleteAsync(string company_id, string invoiceId, CancellationToken cancellationToken);
+
+        /// <summary>URL para Download do PDF da Nota Fiscal de Produto (NFe)</summary>
+        /// <param name="company_id">ID da empresa</param>
+        /// <param name="invoiceId">ID da Nota Fiscal de Produto (NFe)</param>
+        /// <returns>Sucesso na requisição</returns>
+        /// <exception cref="SwaggerException">A server side error occurred.</exception>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        Task<Result<string>> GetDocumentPdfUrlAsync(string company_id, string invoiceId, CancellationToken cancellationToken);
+
+        /// <summary>Download do XML da Nota Fiscal de Produto (NFe)</summary>
+        /// <param name="company_id">ID da empresa</param>
+        /// <param name="invoiceId">ID da Nota Fiscal de Produto (NFe)</param>
+        /// <returns>Sucesso na requisição</returns>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        Task<Result<string>> GetDocumentXmlAsync(string company_id, string invoiceId, CancellationToken cancellationToken);
+    }
+}
--- nfe-api-client/Domain/Models/Product/Invoice.cs
+++ nfe-api-client/Domain/Models/Product/Invoice.cs
@@ -42,6 +42,14 @@ namespace nfe.api.client.Domain.Models.Product
         public DateTimeOffset? ModifiedOn { get; set; }
 
         /// <summary>
+        /// Data e Hora de Saída ou da Entrada da Mercadoria/Produto (dhSaiEnt)
+        /// <remarks>
+        ///     Data e hora no formato UTC (Universal Coordinated Time): AAAA-MM-DDThh:mm:ssTZD.
+        /// </remarks>
+        /// </summary>
+        public DateTimeOffset? OperationOn { get; set; }
+
+        /// <summary>
         /// Tipo de Operação (tpNF)
         /// <remarks>
         ///     0 - Entrada (Incoming)
@@ -49,6 +57,29 @@ namespace nfe.api.client.Domain.Models.Product
         /// </remarks>
         /// </summary>
         public OperationType OperationType { get; set; }
+
+        /// <summary>
+        /// Identificador de local de destino da operação (idDest)s
+        /// <remarks>
+        ///     1 - Operação interna (Internal_Operation)
+        ///     2 - Operação interestadual (Interstate_Operation)
+        ///     3 - Operação com exterior (International_Operation)
+        /// </remarks>
+        /// </summary>
+        public Destination Destination { get; set; }
+
+        /// <summary>
+        /// Formato de Impressão do DANFE (tpImp)
+        /// <remarks>
+        ///     0 - Sem geração de DANFE (None)
+        ///     1 - DANFE normal, Retrato (NFeNormalPortrait)
+        ///     2 - DANFE normal, Paisagem (NFeNormalLandscape)
+        ///     3 - DANFE Simplificado (NFeSimplified)
+        ///     4 - DANFE NFC-e (DANFE_NFC_E)
+        ///     5 - DANFE NFC-e em mensagem eletrônica (DANFE_NFC_E_MSG_ELETRONICA)
+        /// </remarks>
+        /// </summary>
+        public PrintType PrintType { get; set; }
 
         /// <summary>
         /// Identificação do Ambiente (tpAmb)
@@ -65,6 +96,41 @@ namespace nfe.api.client.Domain.Models.Product
         /// </remarks>
         /// </summary>
         public PurposeType PurposeType { get; set; }
+
+        /// <summary>
+        /// Indica operação com Consumidor final  (indFinal)
+        /// <remarks>
+        ///     0 - Normal (Normal)
+        ///     1 - Consumidor final (FinalConsumer)
+        /// </remarks>
+        /// </summary>
+        public ConsumerType ConsumerType { get; set; }
+
+        /// <summary>
+        /// Indicador de presença do comprador no estabelecimento comercial no momento da operação (indPres)
+        /// <remarks>
+        ///     0 - Não se aplica (por exemplo, Nota Fiscal complementar ou de ajuste)
+        ///     1 - Operação presencial
+        ///     2 - Operação não presencial, pela Internet
+        ///     3 - Operação não presencial, Teleatendimento
+        ///     4 - NFC-e em operação com entrega a domicílio
+        ///     9 - Operação não presencial, outros
+        /// </remarks>
+        /// </summary>
+        public ConsumerPresenceType PresenceType { get; set; }
+
+        /// <summary>
+        /// Data e Hora da entrada em contingência (dhCont)
+        /// <remarks>
+        ///     Data e hora no formato UTC (Universal Coordinated Time): AAAA-MM-DDThh:mm:ssTZD
+        /// </remarks>
+        /// </summary>
+        public DateTimeOffset? ContingencyOn { get; set; }
+
+        /// <summary>
+        /// Justificativa da entrada em contingência (xJust)
+        /// </summary>
+        public string ContingencyJustification { get; set; }
 
         /// <summary>
         /// Emitente (emit)
@@ -92,6 +158,11 @@ namespace nfe.api.client.Domain.Models.Product
         public AdditionalInformation AdditionalInformation { get; set; }
 
         /// <summary>
+        /// Detalhamento de Produtos e Serviços (det)
+        /// </summary>
+        public List<InvoiceItem> Items { get; set; }
+
+        /// <summary>
         ///  Grupo Cobrança (cobr)
         /// </summary>
         public Billing Billing { get; set; }
@@ -100,10 +171,5 @@ namespace nfe.api.client.Domain.Models.Product
         ///  Grupo de Formas de Pagamento (pag)
         /// </summary>
         public List<Payment> Payment { get; set; }
-
-        /// <summary>
-        /// Eventos do processamento
-        /// </summary>
-        public InvoiceEvents LastEvents { get; set; }
     }
 }
